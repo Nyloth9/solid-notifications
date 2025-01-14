24c28c8d0d5bb198674c9b250493d8891ac74194
Nyloth9 <markozekanovic1988@gmail.com>
2025-01-14 12:04:31 +0100

    Update helpers.tsx

--- src/utils/helpers.tsx
+++ src/utils/helpers.tsx
@@ -263,8 +263,6 @@ function renderDismissButton(toast: Toast) {
       <span class="sr-only">Close</span>
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        width="22"
-        height="22"
         viewBox="0 0 24 24"
       >
         <path
