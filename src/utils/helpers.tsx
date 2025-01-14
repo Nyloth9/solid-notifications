0f8e7695e51816b74b55e574c8d680e01e40394a
Nyloth9 <markozekanovic1988@gmail.com>
2025-01-14 12:05:52 +0100

    styling updates

--- src/utils/helpers.tsx
+++ src/utils/helpers.tsx
@@ -264,6 +264,8 @@ function renderDismissButton(toast: Toast) {
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
+        width="100%"
+        height="100%"
       >
         <path
           fill="none"
