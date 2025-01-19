e9fa7beadc089768ec67840307440f14ec1aee27
Nyloth9 <96616657+Nyloth9@users.noreply.github.com>
2025-01-19 22:57:20 +0100

    using vite now for build

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [
+    solidPlugin(),
+    dts({ rollupTypes: true, tsconfigPath: "../tsconfig.json" }),
+  ],
   build: {
     target: "esnext",
     lib: {
