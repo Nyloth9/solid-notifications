cf2a9727cc3c21ba2da2d8362868c785a14e28cf
Nyloth9 <96616657+Nyloth9@users.noreply.github.com>
2025-01-24 23:08:10 +0100

    Update intro.tsx

--- website/src/panels/intro.tsx
+++ website/src/panels/intro.tsx
@@ -74,7 +74,7 @@ function CoreFeatures(props: { feature: string }) {
                     <div class="not-prose relative overflow-hidden rounded-md px-4 py-2 text-xs shadow-stripe">
                       Hook into the progress() signal to update the progress bar
                       width or other properties.
-                      <p class="text-xs font-medium text-blue-600 dark:text-emerald-200">
+                      <p class="text-xs font-medium text-emerald-600 dark:text-emerald-200">
                         Current progress:{" "}
                         {Math.round(t.progressManager.progress())}
                       </p>
@@ -87,7 +87,7 @@ function CoreFeatures(props: { feature: string }) {
                     </div>
                     <button
                       onClick={() => t.dismiss()}
-                      class="absolute -right-1 -top-1 h-4 w-4 rounded-full border border-slate-200 bg-white"
+                      class="absolute -right-1.5 -top-1.5 h-5 w-5 rounded-full border border-slate-200 bg-white p-0.5 hover:bg-slate-100 dark:border-slate-700 dark:bg-slate-800 dark:hover:bg-slate-700"
                     >
                       <svg
                         aria-hidden="true"
