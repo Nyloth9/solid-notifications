288db6bb5afb4c4b0c29bcb18312facba07cc520
Nyloth9 <markozekanovic1988@gmail.com>
2025-01-24 11:15:15 +0100

    scrollbar styles

--- website/src/app.tsx
+++ website/src/app.tsx
@@ -13,21 +13,7 @@ export default function App() {
     <ThemeProvider>
       <ToastProvider>
         <Layout>
-          <Router
-            root={(props) => {
-              return (
-                <div class="relative flex h-full flex-col px-4 pt-14 sm:px-6 lg:px-8">
-                  <main class="w-full flex-auto text-left">
-                    <div class="prose dark:prose-invert [html_:where(&>*)]:mx-auto [html_:where(&>*)]:max-w-2xl [html_:where(&>*)]:lg:mx-[calc(50%-min(50%,theme(maxWidth.lg)))] [html_:where(&>*)]:lg:max-w-3xl">
-                      <article class="pb-10 pt-14">
-                        <Suspense>{props.children}</Suspense>
-                      </article>
-                    </div>
-                  </main>
-                </div>
-              );
-            }}
-          >
+          <Router root={(props) => <Suspense>{props.children}</Suspense>}>
             <FileRoutes />
           </Router>
         </Layout>
--- website/src/components/Layout.tsx
+++ website/src/components/Layout.tsx
@@ -386,7 +386,7 @@ export default function Layout(props: Props) {
           </nav>
         </div>
       </header>
-      <div class="relative flex h-full flex-col px-4 pt-14 sm:px-6 lg:px-8">
+      <div class="relative flex h-full max-h-screen scrollbar-md flex-col overflow-y-auto px-4 pt-14 sm:px-6 lg:px-8">
         <main class="w-full flex-auto text-left">
           <div class="prose dark:prose-invert [html_:where(&>*)]:mx-auto [html_:where(&>*)]:max-w-2xl [html_:where(&>*)]:lg:mx-[calc(50%-min(50%,theme(maxWidth.lg)))] [html_:where(&>*)]:lg:max-w-3xl">
             <article class="pb-10 pt-14">{props.children}</article>
