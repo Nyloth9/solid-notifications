09d80519d384a13b095d99f9af7633b2ce1f8cb8
Nyloth9 <96616657+Nyloth9@users.noreply.github.com>
2025-01-25 07:38:51 +0100

    xs

--- website/src/app.tsx
+++ website/src/app.tsx
@@ -6,12 +6,14 @@ import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import { ThemeProvider } from "./util/theme";
 import Layout from "./components/Layout";
-import { MetaProvider } from "@solidjs/meta";
+import { MetaProvider, Link } from "@solidjs/meta";
 
 export default function App() {
   return (
     <MetaProvider>
       <ThemeProvider>
+        <Link rel="icon" href="/favicon.svg" />
+        <Link rel="mask-icon" href="/favicon.svg" color="#3b82f6" />
         <Layout>
           <Router root={(props) => <Suspense>{props.children}</Suspense>}>
             <FileRoutes />
