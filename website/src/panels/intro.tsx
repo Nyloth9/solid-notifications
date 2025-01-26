994fff8e7693cd890c7a152f0c389b1919cf90ca
Nyloth9 <96616657+Nyloth9@users.noreply.github.com>
2025-01-26 17:51:28 +0100

    Update intro.tsx

--- website/src/panels/intro.tsx
+++ website/src/panels/intro.tsx
@@ -182,52 +182,18 @@ function CoreFeatures(props: { feature: string }) {
             notify(
               (t) => (
                 <div
-                  id="toast-message-cta"
-                  class="w-full rounded-lg bg-white p-3 text-gray-500 shadow-stripe dark:bg-gray-800 dark:text-gray-400 dark:shadow-dark"
+                  id="toast-notification"
+                  class="w-[334px] rounded-lg bg-white p-4 pt-2 text-slate-900 shadow-stripe dark:bg-slate-800 dark:text-slate-300"
                   role="alert"
                 >
-                  <div class="flex w-full">
-                    <img
-                      class="h-9 w-9 shrink-0 rounded-full bg-cover"
-                      src="https://s3.eu-central-1.amazonaws.com/www.warsawjs.com/static/images/people/ryan-chenkie.jpg"
-                      alt="Ethan Cross image"
-                    />
-                    <div class="ms-3 text-sm font-normal">
-                      <span class="mb-1 text-sm font-medium text-gray-900 dark:text-white">
-                        Ethan Cross
-                      </span>
-                      <div class="mb-2 text-xs font-normal leading-5">
-                        Your post has been liked by{" "}
-                        <a
-                          href="#"
-                          class="text-blue-600 hover:underline dark:text-blue-500"
-                        >
-                          Ethan Cross
-                        </a>{" "}
-                      </div>
-
-                      <div class="mb-3 w-full rounded-full dark:bg-gray-900 bg-gray-200">
-                        <div
-                          class="h-1 w-full rounded-full bg-blue-500 dark:bg-blue-600"
-                          data-role="progress"
-                          style={{
-                            width: `${100 - t.progressManager.progress()}%`,
-                          }}
-                        />
-                      </div>
-
-                      <a
-                        href="#"
-                        class="inline-flex rounded-lg bg-blue-600 px-2.5 py-0.5 text-center text-xs font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-700 dark:hover:bg-blue-600 dark:focus:ring-blue-800"
-                      >
-                        Reply
-                      </a>
-                    </div>
+                  <div class="mb-2 flex items-center">
+                    <span class="mb-1 text-sm font-semibold text-slate-900 dark:text-white">
+                      New notification
+                    </span>
                     <button
                       type="button"
-                      onClick={() => t.dismiss()}
-                      class="-mx-1.5 -my-1.5 ms-auto inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-white p-1.5 text-gray-400 hover:bg-gray-100 hover:text-gray-900 focus:ring-2 focus:ring-gray-300 dark:bg-gray-800 dark:text-gray-500 dark:hover:bg-gray-700 dark:hover:text-white"
-                      data-dismiss-target="#toast-message-cta"
+                      class="-mx-1.5 -my-1.5 ms-auto inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-white p-1.5 text-slate-400 hover:bg-slate-100 hover:text-slate-900 focus:ring-2 focus:ring-slate-300 dark:bg-slate-800 dark:text-slate-500 dark:hover:bg-slate-700 dark:hover:text-white"
+                      data-dismiss-target="#toast-notification"
                       aria-label="Close"
                     >
                       <span class="sr-only">Close</span>
@@ -248,6 +214,59 @@ function CoreFeatures(props: { feature: string }) {
                       </svg>
                     </button>
                   </div>
+                  <div class="flex items-center">
+                    <div class="relative inline-block shrink-0">
+                      <img
+                        class="h-12 w-12 rounded-full"
+                        src="https://s3.eu-central-1.amazonaws.com/www.warsawjs.com/static/images/people/ryan-chenkie.jpg"
+                        alt="Ryan Chenkie image"
+                      />
+                      <span class="absolute bottom-0 right-0 inline-flex h-6 w-6 items-center justify-center rounded-full bg-blue-600">
+                        <svg
+                          class="h-3 w-3 text-white"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 18"
+                          fill="currentColor"
+                        >
+                          <path
+                            d="M18 4H16V9C16 10.0609 15.5786 11.0783 14.8284 11.8284C14.0783 12.5786 13.0609 13 12 13H9L6.846 14.615C7.17993 14.8628 7.58418 14.9977 8 15H11.667L15.4 17.8C15.5731 17.9298 15.7836 18 16 18C16.2652 18 16.5196 17.8946 16.7071 17.7071C16.8946 17.5196 17 17.2652 17 17V15H18C18.5304 15 19.0391 14.7893 19.4142 14.4142C19.7893 14.0391 20 13.5304 20 13V6C20 5.46957 19.7893 4.96086 19.4142 4.58579C19.0391 4.21071 18.5304 4 18 4Z"
+                            fill="currentColor"
+                          />
+                          <path
+                            d="M12 0H2C1.46957 0 0.960859 0.210714 0.585786 0.585786C0.210714 0.960859 0 1.46957 0 2V9C0 9.53043 0.210714 10.0391 0.585786 10.4142C0.960859 10.7893 1.46957 11 2 11H3V13C3 13.1857 3.05171 13.3678 3.14935 13.5257C3.24698 13.6837 3.38668 13.8114 3.55279 13.8944C3.71889 13.9775 3.90484 14.0126 4.08981 13.996C4.27477 13.9793 4.45143 13.9114 4.6 13.8L8.333 11H12C12.5304 11 13.0391 10.7893 13.4142 10.4142C13.7893 10.0391 14 9.53043 14 9V2C14 1.46957 13.7893 0.960859 13.4142 0.585786C13.0391 0.210714 12.5304 0 12 0Z"
+                            fill="currentColor"
+                          />
+                        </svg>
+                        <span class="sr-only">Message icon</span>
+                      </span>
+                    </div>
+                    <div class="ms-3 text-sm font-normal">
+                      <div class="text-sm font-semibold text-slate-900 dark:text-white">
+                        Ryan Chenkie
+                      </div>
+                      <div class="text-sm font-normal">
+                        commented on your photo
+                      </div>
+                      <span class="text-xs font-medium text-blue-600 dark:text-blue-500">
+                        a few seconds ago
+                      </span>
+                    </div>
+                  </div>
+                  <div class="mt-2 mb-0.5 w-full rounded-full bg-slate-200 dark:bg-slate-900">
+                    <div
+                      class="h-1 w-full rounded-full bg-blue-500 dark:bg-slate-100"
+                      data-role="progress"
+                      style={{
+                        width: `${100 - t.progressManager.progress()}%`,
+                      }}
+                    />
+                  </div>
+
+                  <button class="mt-3 bg-blue-600 rounded px-2 py-0.5 text-xs text-white hover:bg-blue-600 dark:bg-blue-600 dark:hover:bg-blue-700">
+                    View comment
+                    <span class="sr-only">View comment</span>
+                  </button>
                 </div>
               ),
               { toasterId: "toaster-6" },
