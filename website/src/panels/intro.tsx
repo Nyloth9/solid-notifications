2a4ca8c30182ee6401d27a8ff1773ddb41a430e5
Nyloth9 <markozekanovic1988@gmail.com>
2025-01-27 15:10:56 +0100

    Update intro.tsx

--- website/src/panels/intro.tsx
+++ website/src/panels/intro.tsx
@@ -426,7 +426,7 @@ function CoreFeatures(props: { feature: string }) {
               },
             );
           }}
-          class="inline-flex justify-center gap-0.5 overflow-hidden rounded-full px-3 py-0.5 text-sm font-medium text-slate-700 ring-1 ring-inset ring-slate-900/20 hover:bg-slate-900/2.5 hover:text-slate-900 active:bg-slate-900/5 dark:bg-white/5 dark:text-slate-50 dark:ring-white/10 dark:hover:bg-white/10 dark:hover:text-white"
+          class="inline-flex x justify-center gap-0.5 overflow-hidden rounded-full px-3 py-0.5 text-sm font-medium text-slate-700 ring-1 ring-inset ring-slate-900/20 hover:bg-slate-900/2.5 hover:text-slate-900 active:bg-slate-900/5 dark:bg-white/5 dark:text-slate-50 dark:ring-white/10 dark:hover:bg-white/10 dark:hover:text-white"
         >
           Timer management
         </button>
