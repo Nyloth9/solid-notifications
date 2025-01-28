11cc0973b3191b43222f1b79f58d2dfcb6e4d4b6
Nyloth9 <markozekanovic1988@gmail.com>
2025-01-28 15:34:00 +0100

    Update HighlightBlock.tsx

--- website/src/components/HighlightBlock.tsx
+++ website/src/components/HighlightBlock.tsx
@@ -39,7 +39,7 @@ export default function HighlightBlock(props: Props) {
           width="18"
           height="18"
           viewBox="0 0 20 20"
-          class="mt-1 h-4 w-4 flex-none fill-yellow-500 stroke-white dark:fill-yellow-200/20 dark:stroke-yellow-200"
+          class="mt-1 h-4 w-4 flex-none fill-yellow-500 stroke-white dark:stroke-black"
         >
           <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93A10 10 0 0 1 2.93 17.07M9 5v6h2V5zm0 8v2h2v-2z" />
         </svg>
