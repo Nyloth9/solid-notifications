79e9a6382b68bf7a2379c5bb159c9e55b715cec3
Nyloth9 <markozekanovic1988@gmail.com>
2025-02-05 12:55:57 +0100

    Update entry-server.tsx

--- website/src/entry-server.tsx
+++ website/src/entry-server.tsx
@@ -12,6 +12,10 @@ export default createHandler(() => (
               name="viewport"
               content="width=device-width, initial-scale=1"
             />
+            <meta
+              name="google-site-verification"
+              content="9CtUlNXg8Hqy_5fXQOR__oRnvuK2C8ztwdPOu-ATgig"
+            />
             <link rel="icon" href="/favicon.ico" />
             {assets}
           </head>
