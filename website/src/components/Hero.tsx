6dda77792bf6bd834028820e40e5acdfee8c9f70
Nyloth9 <markozekanovic1988@gmail.com>
2025-02-07 14:58:59 +0100

    more hero

--- website/src/components/Hero.tsx
+++ website/src/components/Hero.tsx
@@ -1,7 +1,7 @@
 export default function Hero() {
   return (
     <div class="w-full max-w-4xl -mt-8 pb-6">
-      <img src="/SNHero-variant-A.png" alt="Hero" class="mx-auto w-full" />
+      <img src="/SNHero-variant-G.png" alt="Hero" class="mx-auto w-full" />
     </div>
   );
 }
