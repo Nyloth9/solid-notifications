9d4724eb9fc8e734e62b8198cde5c7e4864fc07c
Nyloth9 <96616657+Nyloth9@users.noreply.github.com>
2025-02-08 09:14:04 +0100

    Update Layout.tsx

--- website/src/components/Layout.tsx
+++ website/src/components/Layout.tsx
@@ -162,7 +162,7 @@ export default function Layout(props: Props) {
                   Find something...
                 </button>
               </div>
-              <div class="flex items-center gap-4 lg:hidden">
+              <div class="flex items-center gap-3 lg:hidden">
                 <button
                   type="button"
                   onClick={() => setSidebarOpen(!sidebarOpen())}
@@ -179,7 +179,7 @@ export default function Layout(props: Props) {
                     <path d="M.5 1h9M.5 8h9M.5 4.5h9" />
                   </svg>
                 </button>
-                <a aria-label="Home" href="/" class="flex items-center gap-3">
+                <a aria-label="Home" href="/" class="flex items-center gap-3 max-w-40">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="22"
@@ -197,7 +197,7 @@ export default function Layout(props: Props) {
                     </g>
                   </svg>
 
-                  <span class="text-sm font-bold dark:text-white">
+                  <span class="text-sm font-bolddark:text-white truncate">
                     Solid Notifications
                   </span>
                 </a>
@@ -228,7 +228,7 @@ export default function Layout(props: Props) {
                   </ul>
                 </nav>
                 <div class="hidden md:block md:h-5 md:w-px md:bg-slate-900/10 md:dark:bg-white/15" />
-                <div class="flex gap-4">
+                <div class="flex gap-3">
                   <div class="contents lg:hidden">
                     <button
                       type="button"
@@ -250,6 +250,25 @@ export default function Layout(props: Props) {
                       </svg>
                     </button>
                   </div>
+
+                  <a
+                    class="flex items-center gap-1 text-sm leading-5 text-slate-600 transition hover:text-slate-900 dark:text-slate-400 dark:hover:text-white"
+                    href="https://github.com/Nyloth9/solid-notifications"
+                    target="_blank"
+                  >
+                    <svg
+                      height="20"
+                      width="20"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33s1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2"
+                      />
+                    </svg>
+                  </a>
+
                   <button
                     type="button"
                     onClick={() => {
