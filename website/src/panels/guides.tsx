fb7b37e741afb9d77a6c6d123f8dce41acd880a1
Nyloth9 <96616657+Nyloth9@users.noreply.github.com>
2025-02-09 19:53:47 +0100

    added keyboard controls guide

---
+++ website/src/panels/guides.tsx
@@ -0,0 +1,17 @@
+import { Toaster, useToast } from "solid-notifications";
+
+export function KeyboardShortcutExample() {
+  const { notify } = useToast();
+
+  return (
+    <div>
+      <Toaster />
+      <button
+        class="inline-flex justify-center gap-0.5 overflow-hidden rounded-full px-3 py-0.5 text-sm font-medium text-slate-700 ring-1 ring-inset ring-slate-900/20 hover:bg-slate-900/2.5 hover:text-slate-900 active:bg-slate-900/5 dark:bg-white/5 dark:text-slate-50 dark:ring-white/10 dark:hover:bg-white/10 dark:hover:text-white"
+        onClick={() => notify()}
+      >
+        Show Toast
+      </button>
+    </div>
+  );
+}
